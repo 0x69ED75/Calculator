47c59a2531225bbd4dfb330de705c37471a1b974
Logan <cb58logan@gmail.com>
2022-07-10 18:19:35 +0100

    Added Binary Conversion Added Error Handling Changed the way Square root operations are handled

--- src/main/java/com/example/calculator/Conversions.java
+++ src/main/java/com/example/calculator/Conversions.java
@@ -3,15 +3,15 @@ package com.example.calculator;
 public class Conversions {
     public static void solveConversion(String x){
         operators.equals(); // carrying out any outstanding operations before attempting to convert values.
+        if(Controller.total < 0){modifyDisplay.errorDisplay(); return;}
 
-        switch(x){
-            case "Binary":
+        switch (x) {
+            case "Binary" -> {
                 Controller.total = Double.parseDouble((Integer.toBinaryString((int) Controller.total)));
                 modifyDisplay.setTotal();
                 Controller.calculationsBox_Static.setText("bin(" + modifyDisplay.getCalculationsBoxText() + ")=" + modifyDisplay.roundTo3.format(Controller.total));
-
-                case "Hexadecimal":
-                System.out.println("Soon");
+            }
+            case "Hexadecimal" -> System.out.println("Soon");
         }
     }
 
